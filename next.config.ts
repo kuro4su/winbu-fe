d201c96f7e0b39287b708a2cedde95d670223cce
Ade Tri Fauzan <adetrifauzan@gmail.com>
2025-11-26 01:56:52 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'winbu.tv',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
